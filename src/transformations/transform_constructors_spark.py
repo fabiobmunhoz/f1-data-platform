123ef9bd31baaba20e79535b2bdf864d2488c5dc
Fabio Munhoz <fabiobmunhoz@icloud.com>
2026-08-26 18:00:51 -0300

    Add PySpark constructor transformation

---
+++ src/transformations/transform_constructors_spark.py
@@ -0,0 +1,64 @@
+import sys
+
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
+
+
+if len(sys.argv) < 2:
+    print("Uso: python transform_constructors_spark.py <season>")
+    sys.exit(1)
+
+SEASON = sys.argv[1]
+
+input_path = f"data/bronze/season={SEASON}/constructors.json"
+output_path = f"data/silver_spark/season={SEASON}/constructors"
+
+
+spark = (
+    SparkSession.builder
+    .appName("F1ConstructorsTransformation")
+    .master("local[*]")
+    .getOrCreate()
+)
+
+
+df_raw = (
+    spark.read
+    .option("multiLine", "true")
+    .json(input_path)
+)
+
+
+constructors = (
+    df_raw
+    .selectExpr("explode(constructors) as constructor")
+    .select(
+        col("constructor.constructorId").alias("constructor_id"),
+        col("constructor.name").alias("name"),
+        col("constructor.nationality").alias("nationality")
+    )
+    .dropDuplicates(["constructor_id"])
+)
+
+
+print("Schema Silver:")
+constructors.printSchema()
+
+constructors.show(
+    truncate=False
+)
+
+
+constructors.write.mode("overwrite").parquet(
+    output_path
+)
+
+
+print()
+print(
+    f"Transformação Spark de construtores "
+    f"concluída para {SEASON}"
+)
+
+
+spark.stop()
