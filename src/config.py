8158492d474f4d8b88d715a8b57fd232500cbbc8
Fabio Munhoz <fabiobmunhoz@icloud.com>
2026-08-31 22:59:51 -0300

    feat: add local and S3 storage configuration

--- src/config.py
+++ src/config.py
@@ -1,7 +1,23 @@
+import os
 from pathlib import Path
 
 
 PROJECT_ROOT = Path(__file__).resolve().parent.parent
+
+DATA_STORAGE = os.getenv(
+    "DATA_STORAGE",
+    "local"
+)
+
+S3_BUCKET = os.getenv(
+    "S3_BUCKET",
+    "f1-data-platform-fabio"
+)
+
+
+# ============================================================
+# LOCAL PATHS
+# ============================================================
 
 DATA_DIR = PROJECT_ROOT / "data"
 
@@ -11,6 +27,14 @@ GOLD_DIR = DATA_DIR / "gold_spark"
 
 
 def get_bronze_path(season, dataset):
+
+    if DATA_STORAGE == "s3":
+        return (
+            f"s3://{S3_BUCKET}/bronze/"
+            f"season={season}/"
+            f"{dataset}.json"
+        )
+
     return (
         BRONZE_DIR
         / f"season={season}"
@@ -19,6 +43,14 @@ def get_bronze_path(season, dataset):
 
 
 def get_silver_path(season, dataset):
+
+    if DATA_STORAGE == "s3":
+        return (
+            f"s3://{S3_BUCKET}/silver/"
+            f"season={season}/"
+            f"{dataset}"
+        )
+
     return (
         SILVER_DIR
         / f"season={season}"
@@ -27,6 +59,14 @@ def get_silver_path(season, dataset):
 
 
 def get_gold_path(season, dataset):
+
+    if DATA_STORAGE == "s3":
+        return (
+            f"s3://{S3_BUCKET}/gold/"
+            f"season={season}/"
+            f"{dataset}"
+        )
+
     return (
         GOLD_DIR
         / f"season={season}"
